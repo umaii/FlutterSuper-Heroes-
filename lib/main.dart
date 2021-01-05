1683ba41d2d96f83cfcc4793a5cbc6b1179c595c
umaii <umairkhaliddeveloper@gmail.com>
2021-01-06 03:12:16 +0500

    yeah this  is first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:superhero_app/screens/mainscreen.dart';
+import 'package:superhero_app/utils/constants.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => ThemeNotifier(),
+      child: Consumer<ThemeNotifier>(
+        builder: (context, ThemeNotifier notifier, child) {
+          return MaterialApp(
+            title: 'SuperHero App',
+            debugShowCheckedModeBanner: false,
+            home: MainScreen(),
+            theme: notifier.dark ? Constants.darkTheme : Constants.lightTheme,
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Constants {
+  //App related strings
+  static String appName = "SuperHero App";
+
+  //Colors for theme
+  static Color lightPrimary = Color(0xfff3f4f9);
+  static Color darkPrimary = Color(0xff2B2B2B);
+  static Color lightAccent = Color(0xffffb102);
+  static Color darkAccent = Color(0xffffb102);
+  static Color lightBG = Color(0xfff3f4f9);
+  static Color darkBG = Color(0xff2B2B2B);
+
+  static ThemeData lightTheme = ThemeData(
+    backgroundColor: lightBG,
+    primaryColor: lightPrimary,
+    accentColor: lightAccent,
+    cursorColor: lightAccent,
+    scaffoldBackgroundColor: lightBG,
+    appBarTheme: AppBarTheme(
+      elevation: 0,
+      textTheme: TextTheme(
+        headline6: TextStyle(
+          color: Colors.black,
+          fontSize: 20,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    ),
+  );
+
+  static ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    backgroundColor: darkBG,
+    primaryColor: darkPrimary,
+    accentColor: darkAccent,
+    scaffoldBackgroundColor: darkBG,
+    cursorColor: darkAccent,
+    appBarTheme: AppBarTheme(
+      elevation: 0,
+      textTheme: TextTheme(
+        headline6: TextStyle(
+          color: lightBG,
+          fontSize: 20,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    ),
+  );
+
+  static List<T> map<T>(List list, Function handler) {
+    List<T> result = [];
+    for (var i = 0; i < list.length; i++) {
+      result.add(handler(i, list[i]));
+    }
+
+    return result;
+  }
+}
+
+class ThemeNotifier extends ChangeNotifier {
+  final String key = 'theme';
+  SharedPreferences _prefs;
+  bool _darkTheme;
+  bool get dark => _darkTheme;
+
+  ThemeNotifier() {
+    _darkTheme = true;
+    _loadfromPrefs();
+  }
+  toggleTheme(){
+    _darkTheme = !_darkTheme;
+    _saveToPrefs();
+    notifyListeners();
+  }
+
+  _initPrefs()async{
+    if(_prefs == null)
+      _prefs = await SharedPreferences.getInstance();
+  }
+  _loadfromPrefs()async{
+    await _initPrefs();
+    _darkTheme = _prefs.getBool(key) ?? true;
+    notifyListeners();
+  }
+  _saveToPrefs()async{
+    await _initPrefs();
+    _prefs.setBool(key, _darkTheme);
+  }
+}
